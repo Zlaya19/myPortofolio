d39d933e4e4d4cad45147c45f7fe63ba0ddc5223
Zlatan Gavranovic <zlatangavranovic1906@gmail.com>
2021-09-06 20:03:06 +0200

    adding project 10

---
+++ country.js
@@ -0,0 +1,29 @@
+let input = document.getElementById('inputCountry');
+const btn = document.getElementById('btn');
+const display = document.getElementById('outputCountry');
+
+btn.addEventListener('click', function(){
+    const user = new XMLHttpRequest();
+    user.open('GET', `https://restcountries.eu/rest/v2/name/${input.value}?fullText=true)`, true);
+
+    let output = '';
+
+    user.onload = function(){
+        if(this.status === 200){
+           output = JSON.parse(this.responseText);
+
+            display.innerHTML = `<img src="${output[0].flag}"
+            <div class="headline"><h1 class="h1Style"> ${output[0].name} </h1>
+    
+            <h1> <span>CAPITAL:</span> ${output[0].capital} </h1>
+            <h1> <span>POPULATION:</span>        ${output[0].population} </h1>
+            <h1 class="styleBorder"> <span>BORDERS: </span>       ${output[0].borders} </h1>
+            <h1> <span>LANGUAGE: </span>       ${output[0].languages[0].name} </h1>
+            <h1> <span>CURRENCY: </span>     ${output[0].currencies[0].name} </h1>
+            <div>
+            `;
+        }
+    }
+
+    user.send();
+})
